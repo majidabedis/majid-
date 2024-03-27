580e50a89c01a5643c819ee6639e80cbb1cd78bc
majidabedis <165201659+majidabedis@users.noreply.github.com>
2024-03-27 21:54:38 +0330

    Add files via upload

---
+++ children-productshow.js
@@ -0,0 +1,453 @@
+const product1 = {
+    name: 'Shirt & short Set',
+    price: '$111',
+    href: '#',
+    breadcrumbs: [
+      { id: 1, name: 'children', href: '#' },
+      { id: 2, name: 'Boy', href: '#' },
+      { id: 3, name: 'Clothing', href: '#' },
+    ],
+    images: [
+      {
+        src: '../../../src/assets/children/child-product/1/full.webp',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/1/zoomout.webp',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/1/zoomin.webp',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/1/product.webp',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    colors: [
+      { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+      { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    sizes: [
+      { name: 'XXS', inStock: false },
+      { name: 'XS', inStock: false },
+      { name: 'S', inStock: false },
+      { name: 'M', inStock: true },
+      { name: 'L', inStock: true },
+      { name: 'XL', inStock: true },
+      { name: '2XL', inStock: true },
+      { name: '3XL', inStock: true },
+    ],
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  }
+  export {product1}
+
+//   ****************product2***************** //
+
+  const product2 = {
+    name: 'Shirt',
+    price: '$112.94',
+    href: '#',
+    breadcrumbs: [
+      { id: 1, name: 'Children', href: '#' },
+      { id: 2, name: 'Boy', href: '#' },
+      { id: 3, name: 'Clothing', href: '#' },
+    ],
+    images: [
+      {
+        src: '../../../src/assets/children/child-product/2/full.webp',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/2/zoomin.webp',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/2/zoomin2.webp',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/2/product.webp',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    colors: [
+      { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+      { name: 'Gray', class: 'bg-green-200', selectedClass: 'ring-gray-400' },
+      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    sizes: [
+      { name: 'XXS', inStock: false },
+      { name: 'XS', inStock: false },
+      { name: 'S', inStock: false },
+      { name: 'M', inStock: true },
+      { name: 'L', inStock: true },
+      { name: 'XL', inStock: true },
+      { name: '2XL', inStock: true },
+      { name: '3XL', inStock: true },
+    ],
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  }
+  export {product2}
+
+//   *******product 3 ******************************************//
+
+
+const product3 = {
+    name: 'Shirt',
+    price: '$89.99',
+    href: '#',
+    breadcrumbs: [
+      { id: 1, name: 'children', href: '#' },
+      { id:2, name: 'Girl', href: '#' },
+      { id: 3, name: 'Clothing', href: '#' },
+    ],
+    images: [
+      {
+        src: '../../../src/assets/children/child-product/3/full.webp',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/3/behind.webp',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/3/behind.webp',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: '../../../src/assets/children/child-product/3/zoomout.webp',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    colors: [
+      { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+      { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    sizes: [
+      { name: 'XXS', inStock: false },
+      { name: 'XS', inStock: false },
+      { name: 'S', inStock: false },
+      { name: 'M', inStock: true },
+      { name: 'L', inStock: true },
+      { name: 'XL', inStock: true },
+      { name: '2XL', inStock: true },
+      { name: '3XL', inStock: true },
+    ],
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  }
+  export {product3}
+
+      //***** product 4*********
+
+      const product4 = {
+        name: 'Shirt',
+        price: '$139.99',
+        href: '#',
+        breadcrumbs: [
+            { id: 1, name: 'children', href: '#' },
+            { id:2, name: 'Girl', href: '#' },
+            { id: 3, name: 'Clothing', href: '#' },
+        ],
+        images: [
+          {
+            src: '../../../src/assets/children/child-product/4/full.webp',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/4/zoomin.webp',
+            alt: 'Model wearing plain black basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/4/side.webp',
+            alt: 'Model wearing plain gray basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/4/zoomin.webp',
+            alt: 'Model wearing plain white basic tee.',
+          },
+        ],
+        colors: [
+          { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+          { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+          { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+        sizes: [
+          { name: 'XXS', inStock: false },
+          { name: 'XS', inStock: false },
+          { name: 'S', inStock: false },
+          { name: 'M', inStock: true },
+          { name: 'L', inStock: true },
+          { name: 'XL', inStock: true },
+          { name: '2XL', inStock: true },
+          { name: '3XL', inStock: true },
+        ],
+        description:
+          'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+        highlights: [
+          'Hand cut and sewn locally',
+          'Dyed with our proprietary colors',
+          'Pre-washed & pre-shrunk',
+          'Ultra-soft 100% cotton',
+        ],
+        details:
+          'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      }
+      export {product4}
+
+
+// product 5
+      const product5 = {
+        name: 'Shirt',
+        price: '$219.99',
+        href: '#',
+        breadcrumbs: [
+            { id: 1, name: 'children', href: '#' },
+            { id:2, name: 'Girl', href: '#' },
+            { id: 3, name: 'Clothing', href: '#' },
+        ],
+        images: [
+          {
+            src: '../../../src/assets/children/child-product/5/zoomin.webp',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/5/zoomin.webp',
+            alt: 'Model wearing plain black basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/5/behind.webp',
+            alt: 'Model wearing plain gray basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/5/full.webp',
+            alt: 'Model wearing plain white basic tee.',
+          },
+        ],
+        colors: [
+          { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+          { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+          { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+        sizes: [
+          { name: 'XXS', inStock: false },
+          { name: 'XS', inStock: false },
+          { name: 'S', inStock: false },
+          { name: 'M', inStock: true },
+          { name: 'L', inStock: true },
+          { name: 'XL', inStock: true },
+          { name: '2XL', inStock: true },
+          { name: '3XL', inStock: true },
+        ],
+        description:
+          'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+        highlights: [
+          'Hand cut and sewn locally',
+          'Dyed with our proprietary colors',
+          'Pre-washed & pre-shrunk',
+          'Ultra-soft 100% cotton',
+        ],
+        details:
+          'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      }
+      export {product5}
+
+
+//product 6
+      const product6 = {
+        name: 'Shirt',
+        price: '$349.99',
+        href: '#',
+        breadcrumbs: [
+            { id: 1, name: 'children', href: '#' },
+            { id:2, name: 'Girl', href: '#' },
+            { id: 3, name: 'Clothing', href: '#' },
+        ],
+        
+        images: [
+          {
+            src: '../../../src/assets/children/child-product/6/full.webp',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/6/zoomin.webp',
+            alt: 'Model wearing plain black basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/6/zoomout.webp',
+            alt: 'Model wearing plain gray basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/6/zoomin.webp',
+            alt: 'Model wearing plain white basic tee.',
+          },
+        ],
+        colors: [
+          { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+          { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+          { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+        sizes: [
+          { name: 'XXS', inStock: false },
+          { name: 'XS', inStock: false },
+          { name: 'S', inStock: false },
+          { name: 'M', inStock: true },
+          { name: 'L', inStock: true },
+          { name: 'XL', inStock: true },
+          { name: '2XL', inStock: true },
+          { name: '3XL', inStock: true },
+        ],
+        description:
+          'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+        highlights: [
+          'Hand cut and sewn locally',
+          'Dyed with our proprietary colors',
+          'Pre-washed & pre-shrunk',
+          'Ultra-soft 100% cotton',
+        ],
+        details:
+          'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      }
+      export {product6}
+
+
+      const product7 = {
+        name: 'Shirt',
+        price: '$209.99',
+        href: '#',
+        breadcrumbs: [
+          { id: 1, name: 'Children', href: '#' },
+          { id: 2, name: 'Boy', href: '#' },
+          { id: 3, name: 'Clothing', href: '#' },
+        ],
+        images: [
+          {
+            src: '../../../src/assets/children/child-product/7/full.webp',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/7/full.webp',
+            alt: 'Model wearing plain black basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/7/zoomin.webp',
+            alt: 'Model wearing plain gray basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/7/product1.webp',
+            alt: 'Model wearing plain white basic tee.',
+          },
+        ],
+        colors: [
+          { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+          { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+          { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+        sizes: [
+          { name: 'XXS', inStock: false },
+          { name: 'XS', inStock: false },
+          { name: 'S', inStock: false },
+          { name: 'M', inStock: true },
+          { name: 'L', inStock: true },
+          { name: 'XL', inStock: true },
+          { name: '2XL', inStock: true },
+          { name: '3XL', inStock: true },
+        ],
+        description:
+          'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+        highlights: [
+          'Hand cut and sewn locally',
+          'Dyed with our proprietary colors',
+          'Pre-washed & pre-shrunk',
+          'Ultra-soft 100% cotton',
+        ],
+        details:
+          'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      }
+      export {product7}
+
+
+//product 8
+      const product8 = {
+        name: 'Shirt',
+        price: '$119.99',
+        href: '#',
+        breadcrumbs: [
+          { id: 1, name: 'Men', href: '#' },
+          { id: 2, name: 'Clothing', href: '#' },
+        ],
+        images: [
+          {
+            src: '../../../src/assets/children/child-product/8/full.webp',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/8/product1.webp',
+            alt: 'Model wearing plain black basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/8/side.webp',
+            alt: 'Model wearing plain gray basic tee.',
+          },
+          {
+            src: '../../../src/assets/children/child-product/8/behind.webp',
+            alt: 'Model wearing plain white basic tee.',
+          },
+        ],
+        colors: [
+          { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+          { name: 'Gray', class: 'bg-blue-200', selectedClass: 'ring-gray-400' },
+          { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+        ],
+        sizes: [
+          { name: 'XXS', inStock: false },
+          { name: 'XS', inStock: false },
+          { name: 'S', inStock: false },
+          { name: 'M', inStock: true },
+          { name: 'L', inStock: true },
+          { name: 'XL', inStock: true },
+          { name: '2XL', inStock: true },
+          { name: '3XL', inStock: true },
+        ],
+        description:
+          'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+        highlights: [
+          'Hand cut and sewn locally',
+          'Dyed with our proprietary colors',
+          'Pre-washed & pre-shrunk',
+          'Ultra-soft 100% cotton',
+        ],
+        details:
+          'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      }
+      export {product8}
